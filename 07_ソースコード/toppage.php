7d887b36e6f812212ef9d18d7a9bd8ee776413ca
Aso2001031 <83050649+Aso2001031@users.noreply.github.com>
2021-10-06 12:18:17 +0900

    Create toppage.php

---
+++ 07_ソースコード/toppage.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>文房具サイト</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+
+<input type="button" onclick="location.href='toppage.php'" value="文房具サイト">
+
+<input type="button" onclick="location.href='register.php'" value="会員登録">
+
+<input type="button" onclick="location.href='login.php'" value="ログイン">
+
+<input type="text"><button type="submit">検索</button>
+</body>
+</html>
