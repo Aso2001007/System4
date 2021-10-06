c08c9bd5c3c1dca7622b140806a91f68ec21482d
Aso2001031 <83050649+Aso2001031@users.noreply.github.com>
2021-10-06 12:19:53 +0900

    Create register.php

---
+++ 07_ソースコード/register.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>会員登録</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+
+<input type="button" onclick="location.href='toppage.php'" value="文房具サイト">
+
+
+<input type="text"><button type="submit">検索</button>
+
+</body>
+</html>
