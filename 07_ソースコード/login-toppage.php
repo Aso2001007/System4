753885bf4accbcee005f114b717da9cd0d416b66
Aso2001031 <83050649+Aso2001031@users.noreply.github.com>
2021-10-08 12:28:31 +0900

    Create login-toppage.php

---
+++ 07_ソースコード/login-toppage.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>文房具サイト</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+
+<input type="button" onclick="location.href='login-toppage.php'" value="文房具サイト">
+
+<input type="button" onclick="location.href='cart.php'" value="カートの中">
+
+<input type="button" onclick="location.href='information.php'" value="会員情報">
+
+<input type="button" onclick="location.href='toppage.php'" value="ログアウト">
+
+<input type="text"><button type="submit">検索</button>
+
+</body>
+</html>
