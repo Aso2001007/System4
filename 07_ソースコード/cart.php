b68a058741371e4b6debd76856f289a10559088a
Aso2001031 <83050649+Aso2001031@users.noreply.github.com>
2021-10-08 12:34:14 +0900

    Create cart.php

---
+++ 07_ソースコード/cart.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>カート</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+<input type="button" onclick="location.href='toppage-log.php'" value="文房具サイト">
+
+<input type="button" onclick="location.href='cart.php'" value="カートの中">
+
+<input type="button" onclick="location.href='information.php'" value="会員情報">
+
+<input type="button" onclick="location.href='toppage.php'" value="ログアウト">
+
+<input type="text"><button type="submit">検索</button>
+
+<input type="button" onclick="location.href='order.php'"  value="注文">
+
+</body>
+</html>
