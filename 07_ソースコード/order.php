23137b0010ea3fcc12ff7847902a1a1d5d8202c6
Aso2001031 <83050649+Aso2001031@users.noreply.github.com>
2021-10-11 14:10:31 +0900

    Create order.php

---
+++ 07_ソースコード/order.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>注文</title>
+</head>
+<body>
+<input type="button" onclick="location.href='login-toppage.php'" value="文房具サイト">
+
+<input type="button" onclick="location.href='cart.php'" value="カートの中">
+
+<input type="button" onclick="location.href='information.php'" value="会員情報">
+
+<input type="button" onclick="location.href='toppage.php'" value="ログアウト">
+
+<input type="text"><button type="submit">検索</button>
+
+<input type="button" onclick="location.href='confirmation.php'" value="入力内容の確認">
+
+</body>
+</html>
