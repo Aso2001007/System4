984158390e70469016fdebdd3ef9f2a450a6bd2d
Aso2001031 <83050649+Aso2001031@users.noreply.github.com>
2021-10-12 09:51:00 +0900

    Create login-item.php

---
+++ 07_ソースコード/login-item.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>商品詳細</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+
+<input type="button" onclick="location.href='login-toppage.php'" value="文房具サイト">
+
+<input type="button" onclick="location.href='cart.php'" value="カートの中">
+
+<input type="button" onclick="location.href='information.php'" value="会員情報">
+
+<input type="button" onclick="location.href='toppage.php'" value="ログアウト">
+
+<input type="text"><button type="submit">検索</button>
+
+<input type="button" onclick="location.href='cart.php'" value="カートに入れる">
+
+</body>
+</html>
