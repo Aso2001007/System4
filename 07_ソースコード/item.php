615681593f2d842db58486b8a812d5afb77238bd
Aso2001031 <83050649+Aso2001031@users.noreply.github.com>
2021-10-12 09:50:25 +0900

    Create item.php

---
+++ 07_ソースコード/item.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>商品詳細</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+
+<input type="button" onclick="location.href='toppage.php'" value="文房具サイト">
+
+<input type="button" onclick="location.href='register.php'" value="会員登録">
+
+<input type="button" onclick="location.href='login.php'" value="ログイン">
+
+<input type="text"><button type="submit">検索</button>
+
+<input type="button" onclick="location.href='cart.php'" value="カートに入れる">
+
+</body>
+</html>
