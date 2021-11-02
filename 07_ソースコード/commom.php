b9f847e85ae62ca2b224021d5d00481db2777d1c
Aso2001031 <83050649+Aso2001031@users.noreply.github.com>
2021-11-02 09:48:43 +0900

    Create commom.php

---
+++ 07_ソースコード/commom.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+<div class="head">
+    <a href="toppage-log.php" id="vanner">文房具サイト</a>
+    <button type="submit"  href="" id="cart">カートの中　　🛒</button><br>
+    <button type="submit" id="infor-regster" href="">会員情報</button>
+    <button type="submit" id="log" href="">ログアウト</button>
+    <input type="text" id="keyword" name="keyword" value="キーワードで検索">
+</div>
+
+
+<dl class="category">
+    <dt>カテゴリーで探す</dt>
+    <dt><hr width="210"></dt>
+    <dt><a href="">鉛筆、ペン</a></dt>
+    <dt><hr width="210"></dt>
+    <dt><a href="">消しゴム</a></dt>
+    <dt><hr width="210"></dt>
+</dl>
+
+</div>
+<script src="./script/script.js"></script>
+</body>
+</html>
