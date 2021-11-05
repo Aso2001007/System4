ab59f9a9425df1c4d85fa8b508841b245aec1263
Aso2001027 <83050634+Aso2001027@users.noreply.github.com>
2021-11-05 12:02:01 +0900

    Update login-toppage.php

--- 07_ソースコード/login-toppage.php
+++ 07_ソースコード/login-toppage.php
@@ -1,21 +1,62 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
-    <title>文房具サイト</title>
+    <title>login-toppage</title>
     <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
 
-<input type="button" onclick="location.href='login-toppage.php'" value="文房具サイト">
+<div class="head">
+    <a href="toppage-log.php" id="vanner">文房具サイト</a>
+    <button type="submit"  href="" id="cart">カートの中　　🛒</button><br>
+    <button type="submit" id="infor-regster" href="">会員情報or会員登録</button>
+    <button type="submit" id="log" href="">ログアウトorログイン</button>
+    <input type="text" id="keyword" name="keyword" value="キーワードで検索 　　 🔍 ">
+</div>
 
-<input type="button" onclick="location.href='cart.php'" value="カートの中">
-
-<input type="button" onclick="location.href='information.php'" value="会員情報">
-
-<input type="button" onclick="location.href='toppage.php'" value="ログアウト">
-
-<input type="text"><button type="submit">検索</button>
-
+<dl class="category">
+    <dt>カテゴリーで探す</dt>
+    <dt><hr width="210"></dt>
+    <dt><a href="">鉛筆、ペン</a></dt>
+    <dt><hr width="210"></dt>
+    <dt><a href="">消しゴム</a></dt>
+    <dt><hr width="210"></dt>
+</dl>
+<?php
+$pdo = new PDO('mysql:host=mysql153.phy.lolipop.lan','LAA1290633','daisuke0804');
+$sql = $pdo->query('select * from commodity where ');
+$flg = true;
+echo '<div class="popular"><a href=""></a><div>';//人気の商品
+foreach ($sql as $row){
+    echo '<div class="popularcommodity">';
+    echo '<a href=""></a>';
+    echo $row['commodity_name'], '：';
+    echo $row['money'], '：';
+    echo '</div>';
+    $flg=false;
+}
+if ($flg) {
+    echo '商品が存在しません', '<br>';
+}
+$sql = $pdo->query('select * from commodity where ');
+$flg = true;
+echo '<div class="campaign"><a href=""></a><div>';//キャンペーン商品
+foreach ($sql as $row){
+echo '<div class="campaigncommodity">';
+    echo '<a href=""></a>';
+    echo $row['commodity_name'], '：';
+    echo $row['money'], '：';
+    echo '</div>';
+$flg=false;
+}
+if ($flg) {
+echo '商品が存在しません', '<br>';
+}
+?>
+<script src="./script/script.js"></script>
 </body>
 </html>
