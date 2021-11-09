6cf80383f4e677915cada171630f50bfab464895
Aso2001031 <83050649+Aso2001031@users.noreply.github.com>
2021-11-09 10:01:56 +0900

    Update commom.php

--- 07_ソースコード/commom.php
+++ 07_ソースコード/commom.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!D<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -6,14 +6,16 @@
     <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
+
 <div class="head">
     <a href="toppage-log.php" id="vanner">文房具サイト</a>
+    <a id="user-name">名前　さん</a>
     <button type="submit"  href="" id="cart">カートの中　　🛒</button><br>
     <button type="submit" id="infor-regster" href="">会員情報or会員登録</button>
     <button type="submit" id="log" href="">ログアウトorログイン</button>
-    <input type="text" id="keyword" name="keyword" value="キーワードで検索 　　 🔍 ">
+    <input type="text" id="keyword" name="keyword" placeholder="キーワードで検索">
+    <button type="submit" id="keyword-button">🔍</button>
 </div>
-
 
 <dl class="category">
     <dt>カテゴリーで探す</dt>
@@ -26,5 +28,3 @@
 
 </div>
 <script src="./script/script.js"></script>
-</body>
-</html>
