6f2a3e05d191f9416abff5637d3b70f8487c913f
Aso2001007 <83050889+Aso2001007@users.noreply.github.com>
2021-11-12 11:36:53 +0900

    Update register.php

--- 07_ソースコード/register.php
+++ 07_ソースコード/register.php
@@ -6,23 +6,45 @@
     <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
-<input type="button" onclick="location.href='toppage.php'" value="文房具サイト">
-<input type="text"><button type="submit">検索</button>
-<form action="register.info.php" method="post">
-    <p>会員登録</p>
-    <p>お名前：<br>
-        <input type="text" name="name" size="10" maxlength="40">
-    </p>
-    <p>電話番号：<br>
-        <input type="text" name="tel" size="10" maxlength="40">
-    </p>
-    <p>Eメール：<br>
-        <input type="text" name="email" size="10" maxlength="40">
-    </p>
-    <p>パスワード：<br>
-        <input type="text" name="pass" size="10" maxlength="40">
-    </p>
-    <p><input type="submit" value="完了"></p>
-</form>
+
+<div class="head">
+    <a href="toppage-log.php" id="vanner">文房具サイト</a>
+    <input type="text" id="keyword" name="keyword" placeholder="キーワードで検索">
+    <button type="submit" id="keyword-button">🔍</button>
+</div>
+
+<dl class="category">
+    <dt>カテゴリーで探す</dt>
+    <dt><hr width="210"></dt>
+    <dt><a href="">鉛筆、ペン</a></dt>
+    <dt><hr width="210"></dt>
+    <dt><a href="">消しゴム</a></dt>
+    <dt><hr width="210"></dt>
+</dl>
+<div class="register">
+    <h1>会員登録</h1>
+</div>
+
+
+<div class="register-main">
+    <form action="register.info.php" method="post">
+
+        <p class="reg-name">お名前：<br>
+            <input type="text" class="register-name" size="100" maxlength="500" placeholder="お名前を入力してください">
+        <p class="reg-tel">電話番号：<br>
+            <input type="text" class="register-tel" placeholder="電話番号を入力してください">
+        </p>
+        <p class="reg-email">Eメール：<br>
+            <input type="text" class="register-email" placeholder="メールアドレスを入力してください">
+        </p>
+        <p class="reg-pass">パスワード：<br>
+            <input type="text" class="register-pass" placeholder="パスワードを入力してください">
+        </p>
+    </div>
+<div class="register-submit">
+        <p><input type="submit" value="完了"></p>
+    </form>
+</div>
+
 </body>
 </html>
