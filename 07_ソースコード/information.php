6f035aee0db9c499458504d579077812b2b9f524
Aso2001031 <83050649+Aso2001031@users.noreply.github.com>
2021-11-22 14:18:07 +0900

    Update information.php

--- 07_ソースコード/information.php
+++ 07_ソースコード/information.php
@@ -1,3 +1,4 @@
+<?php session_start();?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,60 +7,55 @@
     <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
-<div class="head">
-    <a href="toppage-log.html" id="vanner">文房具サイト</a>
-    <a id="user-name">名前　さん</a>
-    <button type="submit"  href="cart.php" id="cart">カートの中　　🛒</button><br>
-    <button type="submit" id="infor-regster" href="register.php">会員情報</button>
-    <button type="submit" id="log" href="">ログアウト</button>
-    <input type="search" id="keyword" name="keyword" placeholder="キーワードで検索">
-    <button type="submit" id="keyword-button">🔍</button>
-</div>
+<!--sessionで会員情報を取得-->
+<?php
+$name=$phone=$mail=$password='';
+if (isset($_SESSION['customer'])) {
+    $name=$_SESSION['customer']['name'];
+    $phone=$_SESSION['customer']['phone'];
+    $mail=$_SESSION['customer']['mail'];
+    $password=$_SESSION['customer']['password'];
+}
+/*メニュー*/
+echo '<div class="head">';
+    echo'<a href="toppage-log.html" id="vanner">文房具サイト</a>';/*トップページへ*/
+    echo'<a id="user-name">',$name,'　さん</a>';/*ユーザー名*/
+    echo'<button type="submit"  id="cart">カートの中　　🛒</button><br>';/*カートへ*/
+    echo'<button type="submit" id="infor-regster" href="register.php">会員情報</button>';/*会員情報へ*/
+    echo'<button type="submit" id="log" href="">ログアウト</button>';/*ログアウト*/
+    echo'<input type="search" id="keyword" name="keyword" placeholder="キーワードで検索">';
+    echo'<button type="submit" id="keyword-button">🔍</button>';/*検索ボタン*/
+echo '</div>';
 
-<dl class="category">
-    <dt>カテゴリーで探す</dt>
-    <dt><hr width="210"></dt>
-    <dt><a href="">鉛筆、ペン</a></dt>
-    <dt><hr width="210"></dt>
-    <dt><a href="">消しゴム</a></dt>
-    <dt><hr width="210"></dt>
-</dl>
+/*カテゴリ*/
+echo '<dl class="category">';
+    echo '<dt>カテゴリーで探す</dt>';
+    echo '<dt><hr width="210"></dt>';
+    echo '<dt><a href="">鉛筆、ペン</a></dt>';
+    echo '<dt><hr width="210"></dt>';
+    echo '<dt><a href="">消しゴム</a></dt>';
+    echo '<dt><hr width="210"></dt>';
+echo '</dl>';
 
-<div class="main">
-
-    <p id="title">会員情報</p>
-
-    <?php
-    //DB接続
-    require_once 'DbManager.php';
-    $pdo = getDB();
-
-    echo '<div class="information">
-        <div class="editbox">
-            <a>お名前：</a><input type="text" class="edittext" value="">
-            <button type="submit" class="edit">編集</button><br>
-            <a>設定されている名前</a><br>
-        </div>
-        <div class="editbox">
-            <a>電話番号：</a><input type="text" class="edittext" value="">
-            <button type="submit" class="edit">編集</button><br>
-            <a>設定されている電話番号</a><br>
-        </div>
-        <div class="editbox">
-            <a>Eメール：</a><input type="text" class="edittext" value="">
-            <button type="submit" class="edit">編集</button><br>
-            <a>設定されているEメール</a><br>
-        </div>
-        <div class="editbox">
-            <a>パスワード：</a><input type="text" class="edittext" value="">
-            <button type="submit" class="edit">編集</button><br>
-            <a>設定されているパスワード</a><br>
-        </div>
-    </div>
-
-    <button type="submit"　name="completion" id="completion">完了</button>';
-    ?>
-</div>
+/*会員情報*/
+echo '<p id="title">会員情報</p>';
+echo '<div class="infor-edit">';
+    echo '<div class="infor-editbox"><a>お名前：</a>';
+        echo '<a>',$name,'</a>';
+    echo '</div>';
+    echo '<div class="infor-editbox"><a>電話番号：</a>';
+        echo '<a>',$phone,'</a>';
+    echo '</div>';
+    echo '<div class="infor-editbox"><a>メールアドレス：</a>';
+        echo '<a>',$mail,'</a>';
+    echo '</div>';
+    echo '<div class="infor-editbox"><a>パスワード：</a>';
+        echo '<a>',$password,'</a>';
+    echo '</div>';
+echo '</div>';
+?>
+/*会員情報編集ページへ移動*/
+<button type="submit" onclick="location.href='edit-in.php'" id="comp-edit">編集する</button>';
 <script src="./script/script.js"></script>
 </body>
 </html>
