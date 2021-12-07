30dd7b5729fd3fdec89cea60a2922bc2d5fd9ab1
Aso2001031 <83050649+Aso2001031@users.noreply.github.com>
2021-12-07 11:17:37 +0900

    Add files via upload

---
+++ 07_ソースコード/information.php
@@ -0,0 +1,52 @@
+<?php session_start();?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" href="./css/style.css">
+    </head>
+<body>
+<!--sessionで会員情報を取得-->
+<?php
+$name=$address=$tell=$mail=$password='';
+if (isset($_SESSION['member'])) {
+    $name=$_SESSION['member']['name'];
+    $address=$_SESSION['member']['address'];
+    $tell=$_SESSION['member']['tell'];
+    $mail=$_SESSION['member']['mail'];
+    $password=$_SESSION['member']['password'];
+}
+
+/*メニュー*/
+echo '<div class="head">';
+    echo'<a href="toppage-log.html" id="vanner">文房具サイト</a>';/*トップページへ*/
+echo '</div>';
+
+/*ページ名*/
+echo '<p id="title">会員情報</p>';
+
+/*出力画面*/
+echo '<div class="infor-edit">';
+    echo '<div class="information-box"><a>お名前：</a>';
+        echo '<a class="member-data">',$name,'</a>';
+    echo '</div>';
+    echo '<div class="information-box"><a>電話番号：</a>';
+        echo '<a class="member-data">',$tell,'</a>';
+    echo '</div>';
+    echo '<div class="information-box"><a>メールアドレス：</a>';
+        echo '<a class="member-data">',$mail,'</a>';
+    echo '</div>';
+    echo '<div class="information-box"><a>パスワード：</a>';
+        echo '<a class="member-data">',$password,'</a>';
+    echo '</div>';
+    echo '<div class="information-box"><a>住所：</a>';
+        echo '<a class="member-data">',$address,'</a>';
+    echo '</div>';
+echo '</div>';
+?>
+<!--会員情報編集ページへ移動-->
+<button type="submit" onclick="location.href='edit-in.php'" class="comp-edit">編集</button>
+<script src="./script/script.js"></script>
+</body>
+</html>
