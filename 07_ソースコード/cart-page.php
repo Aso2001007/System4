c7a6b586411510a725951c8ef13e11d84ef7f5d7
Aso2001031 <83050649+Aso2001031@users.noreply.github.com>
2021-12-09 11:34:10 +0900

    Add files via upload

---
+++ 07_ソースコード/cart-page.php
@@ -0,0 +1,19 @@
+<?php session_start();?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" href="./css/style.css">
+    </head>
+    <body>
+<?php
+
+echo '<div class="head">';
+echo'<a href="toppage-log.html" id="vanner">文房具サイト</a>';/*トップページへ*/
+echo '</div>';
+
+require 'cart.php';
+?>
+    </body>
+</html>
