7ef3048eb87a9cd1f64d32c16ac6ff0204bc4d20
Aso2001007 <83050889+Aso2001007@users.noreply.github.com>
2021-12-09 11:33:37 +0900

    Update comfirmation.php

--- 07_ソースコード/comfirmation.php
+++ 07_ソースコード/comfirmation.php
@@ -2,20 +2,24 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>入力確認</title>
+    <title>購入完了</title>
     <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/comp.css">
 </head>
 <body>
-<input type="button" onclick="location.href='login-toppage.php'" value="文房具サイト">
 
-<input type="button" onclick="location.href='cart.php'" value="カートの中">
+<div class="head">
+    <a href="toppage-log.php" id="vanner">文房具サイト</a>
+</div>
+<div class="comp-h3">
+    <h3>購入完了</h3>
+</div>
 
-<input type="button" onclick="location.href='information.php'" value="会員情報">
+<div class="comp-main">
+    <h1 class="comp-h1">購入が完了しました</h1>
+    <p class="comp-p">ご注文ありがとうございました。</p>
+    <button type="submit" class="comp" onclick=location.href="./login-toppage.php">戻る</button>
+</div>
 
-<input type="button" onclick="location.href='toppage.php'" value="ログアウト">
-
-<input type="text"><button type="submit">検索</button>
-
-<input type="button" onclick="location.href='completion.php'" value="注文確定">
 </body>
 </html>
