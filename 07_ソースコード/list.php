3ae5b0bd4edfe9606f012d3330c31dbf52d74105
Aso2001027 <83050634+Aso2001027@users.noreply.github.com>
2021-12-13 09:43:42 +0900

    Update list.php

--- 07_ソースコード/list.php
+++ 07_ソースコード/list.php
@@ -1,41 +1,56 @@
-<?php
-session_start();
-?>
 <!DOCTYPE html>
-<html lang="ja">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>toppage</title>
-    <link rel="stylesheet" href="./css/top.css">
+    <title></title>
+    <link rel="stylesheet" href="./css/list.css">
 </head>
 <body>
-<div class="head">
-    <a href="toppage.php" id="vanner">文房具サイト</a>
-    <div id="user-name">名前 <?php echo $_SESSION['member']['name']?>さん</div>
-    <button type="submit"  onclick=location.href="./cart.php" id="cart">カートの中　　🛒</button><br>
-    <button type="submit" id="infor-regster" onclick=location.href="./register.php">会員登録</button>
-    <button type="submit" id="log" onclick=location.href="./login.php">ログイン</button>
-    <form action="list.php" method="get">
+    <div class="head">
+        <a href="toppage.php" id="vanner">文房具サイト</a>
+        <button type="submit"  onclick=location.href="./login-in.php" id="cart">カートの中　　🛒</button><br>
+        <button type="submit" id="infor-regster" onclick=location.href="./register.php">会員登録</button>
+        <button type="submit" id="log" onclick=location.href="./login.php">ログイン</button>
+        <form action="list.php" method="get">
         <input type="search" id="keyword" name="keyword" placeholder="キーワードで検索">
         <button type="submit" id="keyword-button">🔍</button>
-</div>
+    </div>
 <dl class="category">
     <dt id="categorysearch">カテゴリーで探す</dt>
     <dt><hr width="210"></dt>
-    <dt><a href="list.php?id=1">鉛筆、ペン</a></dt>
+    <dt><a href="list.php?sql=1">鉛筆、ペン</a></dt>
     <dt><hr width="210"></dt>
-    <dt><a href="list.php?id=2">消しゴム</a></dt>
+    <dt><a href="list.php?sql=2">消しゴム</a></dt>
     <dt><hr width="210"></dt>
 </dl>
+    <div class="pagename">商品一覧</div><div class="sort">並び替え|<a href="list.php?sql=3">価格が安い順</a>|<a href="list.php?sql=4">価格が高い順</a>|<a href="list.php?sql=5">新着順</a></div>
 </form>
 <?php
+error_reporting(0);//ifで選択されなかったSQLがエラーになるから非表示にする(このページのphpはすべて非表示)
 $pdo=new PDO('mysql:host=mysql152.phy.lolipop.lan;dbname=LAA1290633-system4;charset=utf8',
     'LAA1290633','daisuke0804');
-$sql = $pdo->query('select * from item inner join purchase_details on item.item_id = purchase_details.item_id group by purchase_details.item_id having purchase_details.quantity > 0 order by purchase_details.quantity desc, item.item_id asc');
+if (isset($_GET['keyword']) && !isset($_GET['sql'])) {
+    $sql=$pdo->prepare('select * from item where item_name like ? order by item_id asc');
+    $sql->execute(['%'.$_GET['keyword'].'%']);
+} else if($_GET['sql'] == 1){
+    $sql=$pdo->query('select * from item where category_id=1 order by item_id asc');
+} else if($_GET['sql'] == 2){
+    $sql=$pdo->query('select * from item where category_id=2 order by item_id asc');
+} else if($_GET['sql'] == 3){
+    $sql=$pdo->query('select * from item order by price asc, item_id asc');
+} else if($_GET['sql'] == 4){
+    $sql=$pdo->query('select * from item order by price desc, item_id asc');
+} else if($_GET['sql'] == 5){
+    $sql=$pdo->query('select * from item order by date desc, item_id asc');
+} else if($_GET['sql'] == 6){
+    $sql=$pdo->query('select * from item join purchase_details on item.item_id = purchase_details.item_id order by purchase_details.quantity desc, item.item_id asc');
+} else {
+    $sql=$pdo->query('select * from item order by item_id asc');
+}
+$sql->execute();
 $flg = true;
-echo '<div id="popular"><a href="list.php?sql=6"><img src="./img/人気.jpg" width="580px" height="325px"></a><div>';//人気の商品
 if($sql){
-    echo '<ul class="popularitem">';
+    echo '<ul class="displayitem">';
 }
 foreach ($sql as $row){
     $item_id=$row['item_id'];
@@ -44,28 +59,7 @@ foreach ($sql as $row){
     echo '<div class="itemname">',$row['item_name'],'</div><br>';
     echo '<div class="price">','¥',$row['price'],'</div><br>';
     echo '</a></li>';
-    $flg = false;
-}
-if ($sql){
-    echo '</ul>';
-}
-if ($flg) {
-    echo '商品が存在しません', '<br>';
-}
-$sql = $pdo->query('select * from item order by date desc,item_id asc');
-$flg = true;
-echo '<div id="campaign"><a href="list.php?id=5"><img src="./img/CP.jpg" width="580px" height="325px"></a><div>';//キャンペーン商品
-if($sql){
-    echo '<ul class="campaignitem">';
-}
-foreach ($sql as $row){
-    $item_id=$row['item_id'];
-    echo '<li><a href="item.php?id=', $item_id, '">';
-    echo '<img src="./img/',$item_id,'.png"><br>';//商品画像
-    echo '<div class="itemname">',$row['item_name'],'</div><br>';
-    echo '<div class="price">','¥',$row['price'],'</div><br>';
-    echo '</a></li>';
-    $flg = false;
+    $flg=false;
 }
 if ($sql){
     echo '</ul>';
