3010a41e0d75d6fe328f238aa2df30c3ea154a8f
Aso2001027 <83050634+Aso2001027@users.noreply.github.com>
2021-12-13 14:05:19 +0900

    Update login-list.php

--- 07_ソースコード/login-list.php
+++ 07_ソースコード/login-list.php
@@ -10,24 +10,24 @@ session_start();
 </head>
 <body>
 <div class="head">
-    <a href="toppage.php" id="vanner">文房具サイト</a>
+    <a href="login-toppage.php" id="vanner">文房具サイト</a>
     <div id="user-name">名前 <?php echo $_SESSION['member']['name']?>さん</div>
     <button type="submit"  onclick=location.href="./cart.php" id="cart">カートの中　　🛒</button><br>
     <button type="submit" id="infor-regster" onclick=location.href="./information.php">会員情報</button>
     <button type="submit" id="log" onclick=location.href="./logout.php">ログアウト</button>
-    <form action="list.php" method="get">
+    <form action="login-list.php" method="get">
         <input type="search" id="keyword" name="keyword" placeholder="キーワードで検索">
         <button type="submit" id="keyword-button">🔍</button>
 </div>
 <dl class="category">
     <dt id="categorysearch">カテゴリーで探す</dt>
     <dt><hr width="210"></dt>
-    <dt><a href="list.php?sql=1">鉛筆、ペン</a></dt>
+    <dt><a href="login-list.php?sql=1">鉛筆、ペン</a></dt>
     <dt><hr width="210"></dt>
-    <dt><a href="list.php?sql=2">消しゴム</a></dt>
+    <dt><a href="login-list.php?sql=2">消しゴム</a></dt>
     <dt><hr width="210"></dt>
 </dl>
-<div class="pagename">商品一覧</div><div class="sort">並び替え|<a href="list.php?sql=3">価格が安い順</a>|<a href="list.php?sql=4">価格が高い順</a>|<a href="list.php?sql=5">新着順</a></div>
+<div class="pagename">商品一覧</div><div class="sort">並び替え|<a href="login-list.php?sql=3">価格が安い順</a>|<a href="login-list.php?sql=4">価格が高い順</a>|<a href="login-list.php?sql=5">新着順</a></div>
 </form>
 <?php
 error_reporting(0);//ifで選択されなかったSQLがエラーになるから非表示にする(このページのphpはすべて非表示)
@@ -58,7 +58,7 @@ if($sql){
 }
 foreach ($sql as $row){
     $item_id=$row['item_id'];
-    echo '<li><a href="item.php?id=', $item_id, '">';
+    echo '<li><a href="login-item.php?id=', $item_id, '">';
     echo '<img src="./img/',$item_id,'.png"><br>';//商品画像
     echo '<div class="itemname">',$row['item_name'],'</div><br>';
     echo '<div class="price">','¥',$row['price'],'</div><br>';
