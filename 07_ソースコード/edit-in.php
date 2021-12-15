c53c15dc873d23935700ebdc85d7ed57912c23d6
Aso2001031 <83050649+Aso2001031@users.noreply.github.com>
2021-12-15 13:52:12 +0900

    Update edit-in.php

--- 07_ソースコード/edit-in.php
+++ 07_ソースコード/edit-in.php
@@ -1,4 +1,63 @@
-<?php session_start();?>
+<?php session_start();
+if (isset($_POST["edit"])){
+    /*DB接続*/
+    $pdo = new PDO('mysql:host=mysql154.phy.lolipop.lan;
+            dbname=LAA1290579-system4;charset=utf8',
+        'LAA1290579',
+        'IZUken0626');
+
+    $tell=$_POST['tel'];
+    $mail=$_POST['mail'];
+    $password=$_POST['password'];
+
+    /*電話番号の正規表現*/
+    if (preg_match('/^[0-9]{2,4}[0-9]{2,4}[0-9]{3,4}$/',$tel)){
+        /*メールアドレスの正規表現*/
+        if (preg_match('|^[0-9a-z_./?-]+@([0-9a-z]+\.)+[0-9a-z-]+$|',$mail)){
+            /*パスワードの正規表現*/
+            if (preg_match('/(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]{8,24}/',$password)){
+
+                if (isset($_SESSION['member'])) {
+                    $id=$_SESSION['member']['id'];
+                    $sql=$pdo->prepare('SELECT * FROM member WHERE user_id=?');
+                    $sql->execute([$id]);
+                }
+
+                if (isset($_SESSION['member'])) {
+                    $sql=$pdo->prepare('UPDATE member SET name=?,address=?,tel=?,mail=?,pass=? WHERE user_id=?');
+                    $sql->execute([
+                        $_REQUEST['name'],$_REQUEST['address'],$_REQUEST['tel'],
+                        $_REQUEST['mail'],$_REQUEST['password'],$id]);
+
+                    /*sessionに再登録*/
+                    $_SESSION['member']=[
+                        'id'=>$id,'name'=>$_REQUEST['name'],'address'=>$_REQUEST['address'],
+                        'tell'=>$_REQUEST['tel'],'mail'=>$_REQUEST['mail'],
+                        'password'=>$_REQUEST['password']];
+
+                    if (isset($_SESSION['member'])){
+                        http_response_code(301);
+
+                        header("location:http://aso2001007.versus.jp/System4/information.php");
+                        exit;
+                    }else{
+                        echo '<a>お客様情報を更新できませんでした</a>';
+                    }
+                } else {
+                    echo '<a>お客様情報を更新できませんでした。</a>';
+                }
+            }else{
+                echo '<a>お客様情報を更新できませんでした。</a>';
+            }
+        }else{
+            echo '<a>お客様情報を更新できませんでした。</a>';
+        }
+    }else{
+        echo '<a>お客様情報を更新できませんでした。</a>';
+    }
+}
+
+?>
 <!--会員情報編集ページ-->
 <!DOCTYPE html>
 <html lang="en">
@@ -27,7 +86,7 @@ echo '</div>';
 
 /*会員情報*/
 echo '<p class="information_title">会員情報</p>';
-echo '<form action="edit-out.php" method="post">';
+echo '<form action="edit-in.php" method="post">';
     echo '<div class="infor-edit">';
         echo '<div class="edit-box"><a>お名前：</a>';
             echo '<input type="text" name="name" class="edittext" value="',$name,'">';
@@ -46,7 +105,7 @@ echo '<form action="edit-out.php" method="post">';
         echo '</div>';
     echo '</div>';
 ?>
-<button type="submit" class="comp-edit">完了</button>
+<button type="submit" name="edit" class="comp-edit">完了</button>
 </form>
 <script src="./script/script.js"></script>
 </body>
