0a40d654e45ddb912d9a6fc55ca42de7a31c7ad9
Aso2001027 <83050634+Aso2001027@users.noreply.github.com>
2021-12-15 12:17:34 +0900

    Add files via upload

--- 07_ソースコード/login-toppage.php
+++ 07_ソースコード/login-toppage.php
@@ -5,7 +5,7 @@ session_start();
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
-    <title>login-toppage</title>
+    <title>toppage</title>
     <link rel="stylesheet" href="./css/top.css">
 </head>
 <body>
@@ -19,21 +19,29 @@ session_start();
         <input type="search" id="keyword" name="keyword" placeholder="キーワードで検索">
         <button type="submit" id="keyword-button">🔍</button>
 </div>
-<dl class="category">
-    <dt id="categorysearch">カテゴリーで探す</dt>
-    <dt><hr width="210"></dt>
-    <dt><a href="login-list.php?sql=1">鉛筆、ペン</a></dt>
-    <dt><hr width="210"></dt>
-    <dt><a href="login-list.php?sql=2">消しゴム</a></dt>
-    <dt><hr width="210"></dt>
-</dl>
+    <dl class="category">
+        <dt id="all"><a href="login-list.php?default=1">全ての商品</a></dt>
+        <dt><hr width="210"></dt>
+        <dt id="sortname">カテゴリーで探す</dt>
+        <dt><hr width="210"></dt>
+        <dt class="sort"><a href="login-list.php?sql=1">鉛筆、ペン</a></dt>
+        <dt><hr width="210"></dt>
+        <dt class="sort"><a href="login-list.php?sql=2">消しゴム</a></dt>
+        <dt><hr width="210"></dt>
+        <dt class="sort"><a href="login-list.php?sql=3">定規</a></dt>
+        <dt><hr width="210"></dt>
+        <dt class="sort"><a href="login-list.php?sql=4">筆箱</a></dt>
+        <dt><hr width="210"></dt>
+        <dt class="sort"><a href="login-list.php?sql=5">雑貨</a></dt>
+        <dt><hr width="210"></dt>
+    </dl>
 </form>
 <?php
 $pdo=new PDO('mysql:host=mysql152.phy.lolipop.lan;dbname=LAA1290633-system4;charset=utf8',
     'LAA1290633','daisuke0804');
 $sql = $pdo->query('select * from item inner join purchase_details on item.item_id = purchase_details.item_id group by purchase_details.item_id having purchase_details.quantity > 0 order by purchase_details.quantity desc, item.item_id asc');
 $flg = true;
-echo '<div id="popular"><a href="login-list.php?sql=6"><img src="./img/人気.jpg" width="580px" height="325px"></a><div>';//人気の商品
+echo '<div id="popular"><a href="login-list.php?sql=9"><img src="./img/人気.jpg" width="580px" height="325px"></a><div>';//人気の商品
 if($sql){
     echo '<ul class="popularitem">';
 }
@@ -41,8 +49,8 @@ foreach ($sql as $row){
     $item_id=$row['item_id'];
     echo '<li><a href="login-item.php?id=', $item_id, '">';
     echo '<img src="./img/',$item_id,'.png"><br>';//商品画像
-    echo '<div class="itemname">',$row['item_name'],'</div><br>';
-    echo '<div class="price">','¥',$row['price'],'</div><br>';
+    echo '<div class="itemname">',$row['item_name'],'</div>';
+    echo '<div class="price">','¥',$row['price'],'</div>';
     echo '</a></li>';
     $flg = false;
 }
@@ -54,7 +62,7 @@ if ($flg) {
 }
 $sql = $pdo->query('select * from item order by date desc,item_id asc');
 $flg = true;
-echo '<div id="campaign"><a href="login-list.php?sql=5"><img src="./img/CP.jpg" width="580px" height="325px"></a><div>';//キャンペーン商品
+echo '<div id="campaign"><a href="login-list.php?sql=10"><img src="./img/CP.jpg" width="580px" height="325px"></a><div>';//キャンペーン商品
 if($sql){
     echo '<ul class="campaignitem">';
 }
@@ -62,8 +70,8 @@ foreach ($sql as $row){
     $item_id=$row['item_id'];
     echo '<li><a href="login-item.php?id=', $item_id, '">';
     echo '<img src="./img/',$item_id,'.png"><br>';//商品画像
-    echo '<div class="itemname">',$row['item_name'],'</div><br>';
-    echo '<div class="price">','¥',$row['price'],'</div><br>';
+    echo '<div class="itemname">',$row['item_name'],'</div>';
+    echo '<div class="price">','¥',$row['price'],'</div>';
     echo '</a></li>';
     $flg = false;
 }
