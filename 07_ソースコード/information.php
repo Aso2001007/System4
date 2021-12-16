de1fff7dc8b12ab8cf0ab8f1364b01137d829e8d
Aso2001031 <83050649+Aso2001031@users.noreply.github.com>
2021-12-16 11:02:44 +0900

    Update information.php

--- 07_ソースコード/information.php
+++ 07_ソースコード/information.php
@@ -5,7 +5,6 @@
     <head>
         <meta charset="UTF-8">
         <title>文房具サイト</title>
-        <link rel="stylesheet" href="./css/style.css">
         <link rel="stylesheet" href="./css/edit.css">
     </head>
 <body>
